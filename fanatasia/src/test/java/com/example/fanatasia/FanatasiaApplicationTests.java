43472e91772a8433b7decff636313a79836f01fa
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-13 16:01:25 +0100

    zzz

---
+++ fanatasia/src/test/java/com/example/fanatasia/FanatasiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fanatasia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanatasiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
