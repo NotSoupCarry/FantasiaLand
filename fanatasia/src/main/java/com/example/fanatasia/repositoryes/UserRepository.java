b0fc726090d94ebabc922851e19d175c2c83730e
ToninoDB <tonyrussomando@gmail.com>
2025-03-13 17:19:56 +0100

    Aggiunta parte antonio
    Aggiunta controller dell'admin.
    Aggiunta metodi dell'admin nelle service creature e user.

--- fanatasia/src/main/java/com/example/fanatasia/repositoryes/UserRepository.java
+++ fanatasia/src/main/java/com/example/fanatasia/repositoryes/UserRepository.java
@@ -1,8 +1,14 @@
 package com.example.fanatasia.repositoryes;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.fanatasia.models.User;
+import com.example.fanatasia.models.enums.Role;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    List<User> findByRole(Role role);
 }
--- fanatasia/src/main/java/com/example/fanatasia/services/UserService.java
+++ fanatasia/src/main/java/com/example/fanatasia/services/UserService.java
@@ -1,5 +1,35 @@
 package com.example.fanatasia.services;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.example.fanatasia.models.User;
+import com.example.fanatasia.models.enums.Role;
+import com.example.fanatasia.repositoryes.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
-    
+    private final UserRepository userRepository;
+
+    /**
+     * Recupera un custode in base al suo ID.
+     * 
+     * @param id l'id del custode
+     * @return il custode trovato
+     * @throws RuntimeException se il custode non viene trovato
+     */
+    public User findCustodeById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Custode non trovata con id: " + id));
+    }
+
+    /**
+     * Restituisce la lista delle creature disponibili.
+     * 
+     * @return lista delle creature
+     */
+    public List<User> findAllCustodi() {
+        return userRepository.findByRole(Role.CUSTODE);
+    }
 }
