0f9d7f0e070fd01ca6e3c091973f2bf4317483df
gennaro-97 <58686645+gennaro-97@users.noreply.github.com>
2025-03-13 17:46:51 +0100

    Implementazione CreatureService/Controller

--- fanatasia/src/main/java/com/example/fanatasia/services/CreatureService.java
+++ fanatasia/src/main/java/com/example/fanatasia/services/CreatureService.java
@@ -1,5 +1,22 @@
 package com.example.fanatasia.services;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.fanatasia.models.Creature;
+import com.example.fanatasia.models.User;
+import com.example.fanatasia.repositoryes.CreatureRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CreatureService {
     
+    private final CreatureRepository creatureRepository;
+
+    public List<Creature> getAllCreatures(User custode) {
+        return creatureRepository.findByCustode(custode);
+    }
 }
